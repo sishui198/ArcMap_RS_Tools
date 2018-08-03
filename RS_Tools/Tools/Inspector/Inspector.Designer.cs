e58cf748493ca1e17d81b2e6cb38380833218b8e
Chris Stayte <ctstayte@gmail.com>
2018-08-03 08:40:57 -0400

    Refreshed Inspector Dock

--- RS_Tools/Tools/Inspector/Inspector.Designer.cs
+++ RS_Tools/Tools/Inspector/Inspector.Designer.cs
@@ -37,23 +37,19 @@
             this.btnOKNext = new System.Windows.Forms.Button();
             this.btnInitialize = new System.Windows.Forms.Button();
             this.cboBuildingLayer = new System.Windows.Forms.ComboBox();
-            this.rb100 = new System.Windows.Forms.RadioButton();
-            this.rb125 = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.rb150 = new System.Windows.Forms.RadioButton();
-            this.rb200 = new System.Windows.Forms.RadioButton();
-            this.rb250 = new System.Windows.Forms.RadioButton();
-            this.rb300 = new System.Windows.Forms.RadioButton();
-            this.rb350 = new System.Windows.Forms.RadioButton();
-            this.rb400 = new System.Windows.Forms.RadioButton();
+            this.lb_scaleRatio = new System.Windows.Forms.Label();
+            this.tb_scale = new System.Windows.Forms.TrackBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_scale)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_CopyStatus
             // 
             this.btn_CopyStatus.Image = ((System.Drawing.Image)(resources.GetObject("btn_CopyStatus.Image")));
-            this.btn_CopyStatus.Location = new System.Drawing.Point(6, 405);
+            this.btn_CopyStatus.Location = new System.Drawing.Point(3, 316);
             this.btn_CopyStatus.Name = "btn_CopyStatus";
-            this.btn_CopyStatus.Size = new System.Drawing.Size(131, 33);
+            this.btn_CopyStatus.Size = new System.Drawing.Size(155, 33);
             this.btn_CopyStatus.TabIndex = 15;
             this.btn_CopyStatus.UseVisualStyleBackColor = true;
             this.btn_CopyStatus.Click += new System.EventHandler(this.btn_CopyStatus_Click);
@@ -61,7 +57,7 @@
             // lblReport
             // 
             this.lblReport.AutoSize = true;
-            this.lblReport.Location = new System.Drawing.Point(3, 15);
+            this.lblReport.Location = new System.Drawing.Point(3, 0);
             this.lblReport.Name = "lblReport";
             this.lblReport.Size = new System.Drawing.Size(96, 17);
             this.lblReport.TabIndex = 14;
@@ -72,9 +68,9 @@
             this.btnOKStay.ForeColor = System.Drawing.Color.Blue;
             this.btnOKStay.Image = ((System.Drawing.Image)(resources.GetObject("btnOKStay.Image")));
             this.btnOKStay.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOKStay.Location = new System.Drawing.Point(6, 329);
+            this.btnOKStay.Location = new System.Drawing.Point(3, 240);
             this.btnOKStay.Name = "btnOKStay";
-            this.btnOKStay.Size = new System.Drawing.Size(134, 32);
+            this.btnOKStay.Size = new System.Drawing.Size(155, 32);
             this.btnOKStay.TabIndex = 13;
             this.btnOKStay.Text = "OK, Stay";
             this.btnOKStay.UseVisualStyleBackColor = true;
@@ -85,11 +81,11 @@
             this.btnOKNextScale.ForeColor = System.Drawing.Color.CadetBlue;
             this.btnOKNextScale.Image = ((System.Drawing.Image)(resources.GetObject("btnOKNextScale.Image")));
             this.btnOKNextScale.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOKNextScale.Location = new System.Drawing.Point(6, 291);
+            this.btnOKNextScale.Location = new System.Drawing.Point(3, 202);
             this.btnOKNextScale.Name = "btnOKNextScale";
-            this.btnOKNextScale.Size = new System.Drawing.Size(134, 32);
+            this.btnOKNextScale.Size = new System.Drawing.Size(152, 32);
             this.btnOKNextScale.TabIndex = 12;
-            this.btnOKNextScale.Text = "OK, Scale";
+            this.btnOKNextScale.Text = "OK, Same Scale";
             this.btnOKNextScale.UseVisualStyleBackColor = true;
             this.btnOKNextScale.Click += new System.EventHandler(this.btnOKNextScale_Click);
             // 
@@ -98,9 +94,9 @@
             this.btnNext.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
             this.btnNext.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnNext.Location = new System.Drawing.Point(6, 367);
+            this.btnNext.Location = new System.Drawing.Point(3, 278);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(134, 32);
+            this.btnNext.Size = new System.Drawing.Size(155, 32);
             this.btnNext.TabIndex = 11;
             this.btnNext.Text = "Delete";
             this.btnNext.UseVisualStyleBackColor = true;
@@ -111,9 +107,9 @@
             this.btnOKNext.ForeColor = System.Drawing.Color.Green;
             this.btnOKNext.Image = ((System.Drawing.Image)(resources.GetObject("btnOKNext.Image")));
             this.btnOKNext.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOKNext.Location = new System.Drawing.Point(6, 253);
+            this.btnOKNext.Location = new System.Drawing.Point(3, 164);
             this.btnOKNext.Name = "btnOKNext";
-            this.btnOKNext.Size = new System.Drawing.Size(134, 32);
+            this.btnOKNext.Size = new System.Drawing.Size(152, 32);
             this.btnOKNext.TabIndex = 10;
             this.btnOKNext.Text = "OK, Next";
             this.btnOKNext.UseVisualStyleBackColor = true;
@@ -123,9 +119,9 @@
             // 
             this.btnInitialize.Image = ((System.Drawing.Image)(resources.GetObject("btnInitialize.Image")));
             this.btnInitialize.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnInitialize.Location = new System.Drawing.Point(6, 43);
+            this.btnInitialize.Location = new System.Drawing.Point(3, 20);
             this.btnInitialize.Name = "btnInitialize";
-            this.btnInitialize.Size = new System.Drawing.Size(134, 29);
+            this.btnInitialize.Size = new System.Drawing.Size(155, 29);
             this.btnInitialize.TabIndex = 9;
             this.btnInitialize.Text = "Initialize";
             this.btnInitialize.UseVisualStyleBackColor = true;
@@ -136,127 +132,64 @@
             this.cboBuildingLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboBuildingLayer.DropDownWidth = 400;
             this.cboBuildingLayer.FormattingEnabled = true;
-            this.cboBuildingLayer.Location = new System.Drawing.Point(6, 78);
+            this.cboBuildingLayer.Location = new System.Drawing.Point(3, 55);
             this.cboBuildingLayer.Name = "cboBuildingLayer";
-            this.cboBuildingLayer.Size = new System.Drawing.Size(134, 24);
+            this.cboBuildingLayer.Size = new System.Drawing.Size(155, 24);
             this.cboBuildingLayer.TabIndex = 8;
             this.cboBuildingLayer.SelectedIndexChanged += new System.EventHandler(this.cboBuildingLayer_SelectedIndexChanged);
             // 
-            // rb100
+            // lb_scaleRatio
             // 
-            this.rb100.AutoSize = true;
-            this.rb100.Checked = true;
-            this.rb100.Location = new System.Drawing.Point(9, 138);
-            this.rb100.Name = "rb100";
-            this.rb100.Size = new System.Drawing.Size(69, 21);
-            this.rb100.TabIndex = 16;
-            this.rb100.TabStop = true;
-            this.rb100.Text = "100 %";
-            this.rb100.UseVisualStyleBackColor = true;
+            this.lb_scaleRatio.AutoSize = true;
+            this.lb_scaleRatio.Location = new System.Drawing.Point(3, 82);
+            this.lb_scaleRatio.Name = "lb_scaleRatio";
+            this.lb_scaleRatio.Size = new System.Drawing.Size(96, 17);
+            this.lb_scaleRatio.TabIndex = 18;
+            this.lb_scaleRatio.Text = "Scale Ratio: 1";
             // 
-            // rb125
+            // tb_scale
             // 
-            this.rb125.AutoSize = true;
-            this.rb125.Location = new System.Drawing.Point(9, 165);
-            this.rb125.Name = "rb125";
-            this.rb125.Size = new System.Drawing.Size(65, 21);
-            this.rb125.TabIndex = 17;
-            this.rb125.Text = "125%";
-            this.rb125.UseVisualStyleBackColor = true;
+            this.tb_scale.Location = new System.Drawing.Point(3, 102);
+            this.tb_scale.Maximum = 1000;
+            this.tb_scale.Minimum = 100;
+            this.tb_scale.Name = "tb_scale";
+            this.tb_scale.Size = new System.Drawing.Size(152, 56);
+            this.tb_scale.TabIndex = 25;
+            this.tb_scale.TickFrequency = 25;
+            this.tb_scale.Value = 100;
+            this.tb_scale.ValueChanged += new System.EventHandler(this.tb_scale_ValueChanged);
             // 
-            // label1
+            // flowLayoutPanel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 17);
-            this.label1.TabIndex = 18;
-            this.label1.Text = "OK, Next Scale";
-            // 
-            // rb150
-            // 
-            this.rb150.AutoSize = true;
-            this.rb150.Location = new System.Drawing.Point(9, 192);
-            this.rb150.Name = "rb150";
-            this.rb150.Size = new System.Drawing.Size(65, 21);
-            this.rb150.TabIndex = 19;
-            this.rb150.Text = "150%";
-            this.rb150.UseVisualStyleBackColor = true;
-            // 
-            // rb200
-            // 
-            this.rb200.AutoSize = true;
-            this.rb200.Location = new System.Drawing.Point(9, 219);
-            this.rb200.Name = "rb200";
-            this.rb200.Size = new System.Drawing.Size(65, 21);
-            this.rb200.TabIndex = 20;
-            this.rb200.Text = "200%";
-            this.rb200.UseVisualStyleBackColor = true;
-            // 
-            // rb250
-            // 
-            this.rb250.AutoSize = true;
-            this.rb250.Location = new System.Drawing.Point(84, 138);
-            this.rb250.Name = "rb250";
-            this.rb250.Size = new System.Drawing.Size(65, 21);
-            this.rb250.TabIndex = 21;
-            this.rb250.Text = "250%";
-            this.rb250.UseVisualStyleBackColor = true;
-            // 
-            // rb300
-            // 
-            this.rb300.AutoSize = true;
-            this.rb300.Location = new System.Drawing.Point(84, 165);
-            this.rb300.Name = "rb300";
-            this.rb300.Size = new System.Drawing.Size(65, 21);
-            this.rb300.TabIndex = 22;
-            this.rb300.Text = "300%";
-            this.rb300.UseVisualStyleBackColor = true;
-            // 
-            // rb350
-            // 
-            this.rb350.AutoSize = true;
-            this.rb350.Location = new System.Drawing.Point(84, 192);
-            this.rb350.Name = "rb350";
-            this.rb350.Size = new System.Drawing.Size(65, 21);
-            this.rb350.TabIndex = 23;
-            this.rb350.Text = "350%";
-            this.rb350.UseVisualStyleBackColor = true;
-            // 
-            // rb400
-            // 
-            this.rb400.AutoSize = true;
-            this.rb400.Location = new System.Drawing.Point(84, 219);
-            this.rb400.Name = "rb400";
-            this.rb400.Size = new System.Drawing.Size(65, 21);
-            this.rb400.TabIndex = 24;
-            this.rb400.Text = "400%";
-            this.rb400.UseVisualStyleBackColor = true;
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.lblReport);
+            this.flowLayoutPanel1.Controls.Add(this.btnInitialize);
+            this.flowLayoutPanel1.Controls.Add(this.cboBuildingLayer);
+            this.flowLayoutPanel1.Controls.Add(this.lb_scaleRatio);
+            this.flowLayoutPanel1.Controls.Add(this.tb_scale);
+            this.flowLayoutPanel1.Controls.Add(this.btnOKNext);
+            this.flowLayoutPanel1.Controls.Add(this.btnOKNextScale);
+            this.flowLayoutPanel1.Controls.Add(this.btnOKStay);
+            this.flowLayoutPanel1.Controls.Add(this.btnNext);
+            this.flowLayoutPanel1.Controls.Add(this.btn_CopyStatus);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(203, 428);
+            this.flowLayoutPanel1.TabIndex = 26;
+            this.flowLayoutPanel1.WrapContents = false;
             // 
             // Inspector
             // 
-            this.Controls.Add(this.rb400);
-            this.Controls.Add(this.rb350);
-            this.Controls.Add(this.rb300);
-            this.Controls.Add(this.rb250);
-            this.Controls.Add(this.rb200);
-            this.Controls.Add(this.rb150);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.rb125);
-            this.Controls.Add(this.rb100);
-            this.Controls.Add(this.btn_CopyStatus);
-            this.Controls.Add(this.lblReport);
-            this.Controls.Add(this.btnOKStay);
-            this.Controls.Add(this.btnOKNextScale);
-            this.Controls.Add(this.btnNext);
-            this.Controls.Add(this.btnOKNext);
-            this.Controls.Add(this.btnInitialize);
-            this.Controls.Add(this.cboBuildingLayer);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Inspector";
-            this.Size = new System.Drawing.Size(214, 657);
+            this.Size = new System.Drawing.Size(203, 428);
             this.Load += new System.EventHandler(this.BuildingInspector_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tb_scale)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -270,14 +203,8 @@
         private System.Windows.Forms.Button btnOKNext;
         private System.Windows.Forms.Button btnInitialize;
         private System.Windows.Forms.ComboBox cboBuildingLayer;
-        private System.Windows.Forms.RadioButton rb100;
-        private System.Windows.Forms.RadioButton rb125;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton rb150;
-        private System.Windows.Forms.RadioButton rb200;
-        private System.Windows.Forms.RadioButton rb250;
-        private System.Windows.Forms.RadioButton rb300;
-        private System.Windows.Forms.RadioButton rb350;
-        private System.Windows.Forms.RadioButton rb400;
+        private System.Windows.Forms.Label lb_scaleRatio;
+        private System.Windows.Forms.TrackBar tb_scale;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
